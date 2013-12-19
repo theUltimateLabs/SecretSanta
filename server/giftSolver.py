24afeeee0a56e7e494a788aaa7f69c44b72be7aa
RobB <rob@theultimatelabs.com>
2013-12-18 20:03:40 -0700

    Fixed unit test

--- server/giftSolver.py
+++ server/giftSolver.py
@@ -2,14 +2,14 @@ from constraint import *
 import random,unittest,time
 
 
-def GetGiftSolution(families,history=[],maxDelay=1.0,historyYears=0,restrictWithinFmaily=False):
+def GetGiftSolution(families,history=[],maxDelay=1.0,historyYears=0,restrictWithinFamily=False):
     #Two small to get a solution
     
     problem = Problem()
-    extFamilySet = set()
+    extFamily = set()
     for family in families:
-        extFamilySet |= family
-    if len(extFamilySet) < 2:
+        extFamily |= family
+    if len(extFamily) < 2:
         print 'Must be at least two members'
         return None
     
@@ -25,8 +25,8 @@ def GetGiftSolution(families,history=[],maxDelay=1.0,historyYears=0,restrictWith
                 if gifter in pastSolution:
                     gifterHistory.add(pastSolution[gifter])
             
-            domain = extFamilySet - set(gifter) - gifterHistory
-            if restrictWithinFmaily:
+            domain = extFamily - set(gifter) - gifterHistory
+            if restrictWithinFamily:
                 #print 'gifter',gifter,'family',family,'domain',domain
                 domain -= family
             if len(domain) == 0:
@@ -56,67 +56,77 @@ def GetGiftSolution(families,history=[],maxDelay=1.0,historyYears=0,restrictWith
 class Tester(unittest.TestCase):
 
 
-    def checkSolution(self,scenario,solution,history=[]):
+    def checkSolution(self,families,solution,history=[],historyYears=0,restrictWithinFamily=False):
         if (solution == None):
             return
         
-        if len(scenario) < 2:
+        extFamily = set()
+        for family in families:
+            extFamily |= family
+        
+        if len(extFamily) < 2:
             self.assertIsNone(solution)
             return
         
-        self.assertEqual(len(scenario),len(solution))
+        self.assertEqual(len(extFamily),len(solution))
         giftees = set()
-        for gifter in scenario:
-            self.assertIn(gifter,solution)
-            giftee = solution[gifter]
-            for pastSolution in history:
-                if gifter in pastSolution:
-                    self.assertNotEqual(giftee, pastSolution[gifter])
-            self.assertIn(giftee,scenario)
-            self.assertNotEqual(gifter,giftee)
-            giftees.add(giftee)
-        self.assertEqual(len(giftees),len(scenario))
+        for family in families:
+            for gifter in family:
+                self.assertIn(gifter,solution) #make sure each gifter is in solution
+                giftee = solution[gifter] 
+                #make sure no repeats with history range
+                for pastSolution in history:
+                    if historyYears <= 0: break
+                    historyYears -= 1
+                    if gifter in pastSolution:
+                        self.assertNotEqual(giftee, pastSolution[gifter])
+                self.assertIn(giftee,extFamily)
+                self.assertNotEqual(gifter,giftee) #not giving to self
+                if restrictWithinFamily:
+                    self.assertNotIn(giftee,family) # not giving to smoneone in same family
+                giftees.add(giftee)
+        self.assertEqual(len(giftees),len(extFamily)) #number of figters equals number of giftees
         
     
     def test_problem3(self):
         print 'Testing problem3'
-        scenarios = []
+        familiess = []
         
-        scenarios.append(map(set,["A"]))
-        scenarios.append(map(set,["A","BC"]))
-        scenarios.append(map(set,["A","BC","DEF"]))
-        scenarios.append(map(set,["A","BC","DEF","GHIJ"]))
-        scenarios.append(map(set,["A","BC","DEF","GHIJ","KLMNO"]))
+        familiess.append(map(set,["A"]))
+        familiess.append(map(set,["A","BC"]))
+        familiess.append(map(set,["A","BC","DEF"]))
+        familiess.append(map(set,["A","BC","DEF","GHIJ"]))
+        familiess.append(map(set,["A","BC","DEF","GHIJ","KLMNO"]))
          
-        for scenario in scenarios:
+        for families in familiess:
             history = []
-            for y in range(4):
-                solution = GetGiftSolution(scenario,history,historyYears=3,restrictWithinFmaily=True)
+            for year in range(4):
+                solution = GetGiftSolution(families,history,historyYears=3,restrictWithinFamily=True)
                 if not solution: break
                 print solution
-                #self.checkSolution(scenario,solution,history)
+                self.checkSolution(families,solution,history,historyYears=3,restrictWithinFamily=True)
                 history.append(solution)
                 
                 
     def test_problem2(self):
         print 'Testing problem2'
-        scenarios = map(set,["","A","AB","ABC","ABCD","ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
-        for scenario in scenarios:
+        families = map(set,["","A","AB","ABC","ABCD","ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
+        for family in families:
             history = []
-            for y in range(4):
-                solution = GetGiftSolution([scenario],history,historyYears=3)
+            for year in range(4):
+                solution = GetGiftSolution([family],history,historyYears=3)
                 if not solution: break
                 print solution
-                #self.checkSolution(scenario,solution,history)
+                self.checkSolution([family],solution,history,historyYears=3)
                 history.append(solution)
                 
     def test_problem1(self):
         print 'Testing problem1'
-        scenarios = map(set,["","A","AB","ABC","ABCD","ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
-        for scenario in scenarios:
-            solution = GetGiftSolution([scenario])
+        families = map(set,["","A","AB","ABC","ABCD","ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
+        for family in families:
+            solution = GetGiftSolution([family])
             print solution
-            #self.checkSolution(scenario,solution)
+            self.checkSolution([family],solution)
                 
 
 if __name__ == '__main__':
